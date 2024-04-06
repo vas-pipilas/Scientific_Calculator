055cdfba25ed5c826dd55439d4208901df28e2de
anthPappa <anpa1757@gmail.com>
2024-04-06 14:39:52 +0200

    List order change add change symbol func and start memory fun

--- main_test.py
+++ main_test.py
@@ -2,46 +2,45 @@ import tkinter as tk
 import math
 
 calculation = []
+result = ""  # Define result globally
+memory = 0  # Initialize memory
 trigonometry_menu = None
 
 def add_to_calculation(symbol):
-    global calculation
-    if text_result.get(1.0, "end").strip() == "Maximum Length of 20 Digits Reached":
-        text_result.delete(1.0, "end")
+    global calculation, result
+    if len(result) > 20:
+        result = "Maximum Length of 20 Digits Reached";
     if len(calculation) < 20:
         if callable(symbol):
             # If the symbol is a function, call it with the current text input as its argument
-            result = symbol(calculation)
-            text_result.delete(1.0, "end")
-            text_result.insert(1.0, result)
+            result = str(symbol(calculation))
+            update_display(''.join(calculation), result)
         else:
             # If the symbol is not a function, append it to the calculation
-            calculation += str(symbol)
-            text_calculation.delete(1.0, "end")
-            text_calculation.insert(1.0, calculation)
+            calculation.append(str(symbol))
+            update_display(''.join(calculation), result)
     else:
-        text_calculation.insert(1.0, "3.129E-99")
-        text_result.insert(1.0, "Maximum Length of 20 Digits Reached")
+        update_display("3.129E-99", "Maximum Length of 20 Digits Reached")
         calculation = []
 
 def evaluate_calculation():
-    global calculation
+    global calculation, result
     try:
         calculation_str = ''.join(calculation)
-        calculation = str(eval(calculation_str))
-        text_result.delete(1.0, "end")
-        text_result.insert(1.0, calculation)
+        result = str(eval(calculation_str))
+        update_display(calculation_str, result)
     except:
         clear_field()
-        text_calculation.insert(1.0, "ERROR!!")
+        update_display("ERROR!!", "")
 
 def clear_field():
-    global calculation
+    global calculation, result
     calculation = []
-    text_calculation.delete(1.0, "end")
+    result = ""
+    update_display("", "")
 
 
-def trigonometry_popup_window():
+def trigonometry_flyout_window():
     global trigonometry_menu
     trigonometry_menu = tk.Menu(calculator, tearoff=0)
 
@@ -59,50 +58,91 @@ def trigonometry_popup_window():
     trigonometry_menu.post(calculator.winfo_x() + 150, calculator.winfo_y() + 100)
 
 def handle_trigonometry_command(command):
+    global calculation, result
     if callable(command):  # If command is a function, call it directly
         if command.__name__ == "ToRAD" or command.__name__ == "ToDEG":
-            add_to_calculation(command(0))  # Pass 0 as the argument for degree/radian conversion
+            #add_to_calculation(command(0))  # Pass 0 as the argument for degree/radian conversion
+            result = str(command(0))  
+            update_display(f"{command.__name__}(0)", result)
         else:
             try:
                 # Extract the last value from the calculation list and convert it to a float
                 value = float(calculation[0]) if calculation else 0
-                add_to_calculation(command(value))
+                result = str(command(value))
+                update_display(f"{command.__name__}({value})", result)
+                #add_to_calculation(command(value))
             except ValueError:
-                print("Invalid input. Please enter a valid number.")
+                 clear_field()
+                 update_display(''.join(calculation), "Invalid input. Please enter a valid number.")
     else:  # If command is not a function, it's a value or expression
         add_to_calculation(str(command))
 
+
+def update_display(calculation, result):
+    text_display.delete(1.0, "end")  # Clear the current content
+    text_display.insert("end", f"{calculation}\n{result}") 
+
+def change_sign():
+    global calculation
+    try:
+        # Extract the current number from the calculation string
+        num_str = ''.join(calculation)
+        
+        # Convert the current number to a float
+        num = float(num_str)
+        
+        # Check if the number is an integer
+        is_integer = num.is_integer()
+        
+        # Change the sign of the number
+        num = -num if num >= 0 else abs(num)
+        
+        # Convert the number back to an integer if it was originally an integer
+        if is_integer:
+            num = int(num)
+        
+        # Update the calculation list with the new number
+        calculation = [str(num)]
+        
+        # Update the display with the modified number
+        update_display(''.join(calculation), "")
+    except ValueError:
+        # If the input is not a valid number, clear the field and display an error message
+        clear_field()
+        update_display("Invalid input. Please enter a valid number.", "")
+
+def memory_add():
+    global memory
+    memory += float(result) if result else 0
+
+def memory_subtract():
+    global memory
+    memory -= float(result) if result else 0
+
 calculator = tk.Tk()
 calculator.title("Scientific Calculator by PLHPRO Team :) ")
-calculator.geometry("735x400")
+calculator.geometry("620x337")
 
-text_calculation = tk.Text(calculator, height=1, width=40, font=("Arial", 24))
-text_calculation.pack(fill="both", expand=True)
-text_calculation.grid(columnspan=6)
-
-text_result = tk.Text(calculator, height=1, width=40, font=("Arial", 24))
-text_result.grid(columnspan=6)
+text_display = tk.Text(calculator, height=2, width=40, font=("Arial", 24))
+text_display.grid(columnspan=6)
 
 button_data = [
-    ("MC", lambda: clear_field()), ("MR", lambda: clear_field()),
-    ("M+", lambda: clear_field()), ("M-", lambda: clear_field()),
-    ("MS", lambda: clear_field()), ("Trigonometry", trigonometry_popup_window),
-    ("π", lambda: add_to_calculation(math.pi)), ("e", lambda: add_to_calculation("e")),
-    ("C", clear_field), ("<==", lambda: calculation.pop() if calculation else None),
+    ("MC", lambda: clear_field()), ("MR", lambda: clear_field()), ("M+", lambda: memory_add()), 
+    ("M-", lambda: memory_subtract()), ("MS", lambda: clear_field()), 
+    ("Trigonometry", trigonometry_flyout_window), ("π", lambda: add_to_calculation(math.pi)), 
+    ("e", lambda: add_to_calculation("e")), ("C", clear_field), ("<==", lambda: calculation.pop() if calculation else None),
     ("x2", lambda: add_to_calculation("x2")), ("1/x", lambda: add_to_calculation("1/x")),
-    ("|x|", lambda: add_to_calculation("|x|")), ("mod", lambda: add_to_calculation("mod")),
-    ("√x", lambda: add_to_calculation("√x")), ("(", lambda: add_to_calculation("(")),
-    (")", lambda: add_to_calculation(")")), ("!x", lambda: add_to_calculation("!x")),
-    ("/", lambda: add_to_calculation("/")), ("x^y", lambda: add_to_calculation("^")),
-    ("7", lambda: add_to_calculation("7")), ("8", lambda: add_to_calculation("8")),
-    ("9", lambda: add_to_calculation("9")), ("*", lambda: add_to_calculation("*")),
-    ("4", lambda: add_to_calculation("4")), ("5", lambda: add_to_calculation("5")),
-    ("6", lambda: add_to_calculation("6")), ("-", lambda: add_to_calculation("-")),
-    ("log", lambda: add_to_calculation("log")), ("1", lambda: add_to_calculation("1")),
-    ("2", lambda: add_to_calculation("2")), ("3", lambda: add_to_calculation("3")),
-    ("+", lambda: add_to_calculation("+")), ("ln", lambda: add_to_calculation("ln")),
-    ("+/-", None), ("0", lambda: add_to_calculation("0")), (".", lambda: add_to_calculation(".")),
-    ("=", evaluate_calculation)
+    ("|x|", lambda: add_to_calculation("|x|")), ("exp", lambda: add_to_calculation("exp")), ("mod", lambda: add_to_calculation("mod")),
+    ("√x", lambda: add_to_calculation("√x")), ("(", lambda: add_to_calculation("(")), 
+    (")", lambda: add_to_calculation(")")), ("!x", lambda: add_to_calculation("!x")), ("/", lambda: add_to_calculation("/")),
+    ("x^y", lambda: add_to_calculation("^")), ("7", lambda: add_to_calculation(7)), 
+    ("8", lambda: add_to_calculation(8)),("9", lambda: add_to_calculation(9)), ("*", lambda: add_to_calculation("*")),
+    ("10^x", lambda: add_to_calculation("10^x")), ("4", lambda: add_to_calculation(4)), 
+    ("5", lambda: add_to_calculation(5)), ("6", lambda: add_to_calculation(6)), ("-", lambda: add_to_calculation("-")),
+    ("log", lambda: add_to_calculation("log")),("1", lambda: add_to_calculation(1)),
+    ("2", lambda: add_to_calculation(2)), ("3", lambda: add_to_calculation(3)), ("+", lambda: add_to_calculation("+")),
+    ("ln", lambda: add_to_calculation("ln")), ("+/-", lambda: change_sign()), ("0", lambda: add_to_calculation(0)), 
+    (".", lambda: add_to_calculation(".")), ("=", evaluate_calculation)
 ]
 
 for i, (text, command) in enumerate(button_data):
